a67317b695acde7bb97b9dbadc068ebb3b210e06
KatayR <rasitarslan@outlook.com>
2026-01-18 14:39:15 +0300

    feat(nav): Setup Routes constants and app shell

---
+++ lib/presentation/routes/app_routes.dart
@@ -0,0 +1,14 @@
+class AppRoutes {
+  // Roots
+  static const String splash = '/';
+
+  // Tasks
+  static const String tasks = '/tasks';
+  static const String addTask = '/tasks/add';
+  static const String editTask = '/tasks/edit';
+
+  // Habits
+  static const String habits = '/habits';
+  static const String addHabit = '/habits/add';
+  static const String editHabit = '/habits/edit';
+}
