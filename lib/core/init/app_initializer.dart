4ac0415117068dabd66a497d160d80f47b197a33
KatayR <rasitarslan@outlook.com>
2026-01-18 19:46:31 +0300

    feat(data): Create hive adapters for Task

--- lib/core/init/app_initializer.dart
+++ lib/core/init/app_initializer.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 
+import '../../data/models/task_model.dart';
+
 class AppInitializer {
   static Future<void> init() async {
     WidgetsFlutterBinding.ensureInitialized();
 
     // Initialize Hive
     await Hive.initFlutter();
+    Hive.registerAdapter(TaskModelAdapter());
+
+    // Open Boxes
+    await Hive.openBox<TaskModel>('tasks');
   }
 }
