27630a8e457d3b2deccda63b0a6216f4106c7dc7
KatayR <rasitarslan@outlook.com>
2026-01-18 19:46:31 +0300

    feat(tasks): Connect persistence to task logic

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 import 'package:provider/provider.dart';
 
 import 'core/init/app_initializer.dart';
 import 'core/theme/app_theme.dart';
+import 'data/datasources/task_local_data_source.dart';
+import 'data/models/task_model.dart';
+import 'data/repositories/task_repository.dart';
 import 'presentation/controllers/task_controller.dart';
 import 'presentation/screens/tasks/task_list_screen.dart';
 
 void main() async {
   await AppInitializer.init();
-  runApp(ChangeNotifierProvider(create: (_) => TaskController(), child: const MainApp()));
+
+  // Dependency Injection (Manual for MVP)
+  final taskBox = Hive.box<TaskModel>('tasks');
+  final taskLocalDataSource = TaskLocalDataSource(taskBox);
+  final taskRepository = TaskRepository(taskLocalDataSource);
+
+  runApp(
+    ChangeNotifierProvider(
+      create: (_) => TaskController(taskRepository)..loadTasks(), // Trigger load immediately
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
