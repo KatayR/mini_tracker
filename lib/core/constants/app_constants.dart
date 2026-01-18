15d374184d011b15f2a2bd3ba5d643f0158251bd
KatayR <rasitarslan@outlook.com>
2026-01-18 19:46:31 +0300

    feat(constants): Add app constants, styles, icons, strings, and validators

---
+++ lib/core/constants/app_constants.dart
@@ -0,0 +1,11 @@
+class AppConstants {
+  // Hive Boxes
+  static const String taskBox = 'tasks';
+  static const String habitBox = 'habits';
+
+  // Date Formats
+  static const String dateFormat = 'MMM d, y';
+
+  static const Duration oneDay = Duration(days: 1);
+  static const Duration snackBarDuration = Duration(seconds: 2);
+}
