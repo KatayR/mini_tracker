ac6f380416893648804d59ab59964760f705eb50
KatayR <rasitarslan@outlook.com>
2026-01-18 19:46:33 +0300

    feat(di): Implement Dependency Injection with GetIt

---
+++ lib/core/init/injection_container.dart
@@ -0,0 +1,51 @@
+import 'package:get_it/get_it.dart';
+import 'package:hive/hive.dart';
+import 'package:mini_tracker/data/datasources/habit_local_data_source.dart';
+import 'package:mini_tracker/data/datasources/interfaces/i_crud_data_source.dart';
+import 'package:mini_tracker/data/datasources/remote/mock_generic_remote_data_source.dart';
+import 'package:mini_tracker/data/datasources/task_local_data_source.dart';
+import 'package:mini_tracker/data/models/habit_model.dart';
+import 'package:mini_tracker/data/models/task_model.dart';
+import 'package:mini_tracker/data/repositories/habit_repository.dart';
+import 'package:mini_tracker/data/repositories/task_repository.dart';
+import 'package:mini_tracker/domain/repositories/i_habit_repository.dart';
+import 'package:mini_tracker/domain/repositories/i_task_repository.dart';
+import 'package:mini_tracker/presentation/controllers/habit_controller.dart';
+import 'package:mini_tracker/presentation/controllers/task_controller.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // Features - Task
+  sl.registerFactory(() => TaskController(sl()));
+  sl.registerLazySingleton<ITaskRepository>(
+    () => TaskRepository(
+      localDataSource: sl<TaskLocalDataSource>(),
+      remoteDataSource: sl(instanceName: 'TaskRemoteDataSource'),
+    ),
+  );
+
+  sl.registerLazySingleton<TaskLocalDataSource>(() => TaskLocalDataSource(sl()));
+  sl.registerLazySingleton<ICrudDataSource<TaskModel>>(() => sl<TaskLocalDataSource>());
+
+  sl.registerLazySingleton<MockGenericRemoteDataSource<TaskModel>>(() => MockGenericRemoteDataSource<TaskModel>());
+
+  // Features - Habit
+  sl.registerFactory(() => HabitController(sl()));
+  sl.registerLazySingleton<IHabitRepository>(
+    () => HabitRepository(
+      localDataSource: sl<HabitLocalDataSource>(),
+      remoteDataSource: sl<MockGenericRemoteDataSource<HabitModel>>(),
+    ),
+  );
+
+  sl.registerLazySingleton<HabitLocalDataSource>(() => HabitLocalDataSource(sl()));
+  sl.registerLazySingleton<MockGenericRemoteDataSource<HabitModel>>(() => MockGenericRemoteDataSource<HabitModel>());
+
+  // External
+  final taskBox = Hive.box<TaskModel>('tasks');
+  sl.registerLazySingleton(() => taskBox);
+
+  final habitBox = Hive.box<HabitModel>('habits');
+  sl.registerLazySingleton(() => habitBox);
+}
